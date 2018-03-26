ea0ff043e1889ea18c32bb585c56638eb8ef25d7
Antonio <aalaminosz@gmail.com>
2018-03-26 16:30:22 +0200

    board and function to consolelog it

--- tatami.js
+++ tatami.js
@@ -1,5 +1,26 @@
-/*
-this is like a chessboard where most dragons will move randomly,
-one dragon is alway territorial and look for fights.
-They will fight when they encounter each other in the same position within the board,
-*/
+
+
+
+const BOARD3 = [
+    [ [], [], [] , [] ],
+    [ [], [], ['ñu'], [] ],
+    [ [], [], [], [] ],
+    [ [], [], [], [] ],
+    
+]
+
+let printBoard = (function() {
+    for (var x = 0; x < BOARD3.length; x++) {
+        let line = '';
+        for (var y = 0; y < BOARD3[x].length; y++) {
+            if (BOARD3[x][y].length === 0) {
+                line += ' . ';
+            }
+            else line += ' x ';
+        }
+        console.log(line);
+    }
+}) ();
+
+
+
