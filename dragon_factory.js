8d054c10aa434c65f67e24fd527a9e1f2322cc56
Antonio <aalaminosz@gmail.com>
2018-04-07 17:42:32 +0200

    deleted old code

--- dragon_factory.js
+++ dragon_factory.js
@@ -1,19 +1,8 @@
 const Dragon = require('./class_Dragon');
 
 dragonFactory = {
-    name : '', //can be deleted
-    power : 0, //can be deleted
     stamina : 0,
     defense : 0,
-
-    setName : function(name) { //can be deleted
-        this.name = name;
-    },
-
-    setPower : function(power) { //can be deleted
-        //value from 1 to 9
-        this.power = power;
-    },
 
     setStamina : function(power) {
         //the more power the less stamina
@@ -22,9 +11,8 @@ dragonFactory = {
 
     setDefense : function() {
         //big difference between power & stamina will result in low defense, and vice versa
-        let equilibrium = Math.abs(this.power - this.stamina);
-        this.defense = 8 - equilibrium;
-
+        let difference = Math.abs(this.power - this.stamina); //difference between power and stamina, absolute value (no negative numbers)
+        this.defense = 8 - difference;
     },
 
     construct : function(name, power) {
